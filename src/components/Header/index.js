b574fcfe1be0ae4a40cfaeec5e228d0b91486e5c
Lucasedusi <lucasmto.16@hotmail.com>
2020-07-16 14:27:14 -0300

    create main

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import './styles.css';
 
-const Header = () => <header id="main-header">JsHunt</header>;
+const Header = () => <header id="main-header">HuntJs</header>;
 
 export default Header;
